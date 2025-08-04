2ac132865d89f299aac680250034dfbdc7e3488d
programmer1o1 <rtrrt2996@gmail.com>
2025-08-04 14:14:28 +0800

    fix github pages thing

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
   },
   output: {
     target: 'web',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/CGE7-193MapViewer/' : '/',
     // Mark Node.js built-in modules as external.
     externals: ['fs', 'path', 'url'],
     // TODO: These should be converted to use `new URL('./file.wasm', import.meta.url)`
