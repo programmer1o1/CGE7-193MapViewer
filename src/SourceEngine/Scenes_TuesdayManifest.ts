7c5bcf3e59dadda248c71ed9532055577624a885
qbyte <80366805+TheQuantumFrog@users.noreply.github.com>
2025-08-04 21:52:37 -0500

    added hyphen to Half-Life 2

--- src/SourceEngine/Scenes_TuesdayManifest.ts
+++ src/SourceEngine/Scenes_TuesdayManifest.ts
@@ -18,10 +18,10 @@ const sceneDescs = [
     new TeamFortress2SceneDesc('tf_data5'),
     new TeamFortress2SceneDesc('tf_data6'),
     new TeamFortress2SceneDesc('tf_data7'),
-    "Half Life 2",
+    "Half-Life 2",
     new HalfLife2SceneDesc('hl2_data'),
     
-    "Half Life 2: Episode 2",
+    "Half-Life 2: Episode 2",
     new HalfLife2Ep2SceneDesc('ep2_data'),
     new HalfLife2Ep2SceneDesc('ep2_data2'),
     
