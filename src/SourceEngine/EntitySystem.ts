ae013b5693ce99adbcf57df167b0b1b3499b60b9
Jasper St. Pierre <jstpierre@mecheye.net>
2026-03-02 09:36:31 -0800

    SourceEngine: Tick movements on spawned entities even when we're fetching resources

--- src/SourceEngine/EntitySystem.ts
+++ src/SourceEngine/EntitySystem.ts
@@ -5676,10 +5676,7 @@ export class EntitySystem {
         this.flushCreateQueue();
 
         const spawnStateAction = this.getSpawnStateAction();
-        if (spawnStateAction === SpawnState.FetchingResources) {
-            // Still fetching; nothing to do.
-            return;
-        } else if (spawnStateAction === SpawnState.ReadyForSpawn) {
+        if (spawnStateAction === SpawnState.ReadyForSpawn) {
             // Set all parent relationships so that the origin relationships are correct
             // before calling the spawn method on anything.
             for (let i = 0; i < this.entities.length; i++) {
@@ -5701,6 +5698,9 @@ export class EntitySystem {
         this.debugger.movement(renderContext);
 
         for (let i = 0; i < this.entities.length; i++) {
+            if (this.entities[i].spawnState !== SpawnState.Spawned)
+                continue;
+
             if (!this.entities[i].alive) {
                 this.entities.splice(i--, 1);
                 continue;
